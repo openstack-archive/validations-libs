eb0158c35a73c91f776c88ff0c9375c71dc9f4e8
Mathieu Bultel <mat.bultel@gmail.com>
2020-04-01 15:54:28 +0200

    Make mock import python 2.7 compatible
    In python3.x mock is now part of unittest library.
    For being still compatible with python 2.7 we need to
    catch import error when trying to import unittest.mock
    
    Change-Id: I6aca21bea4f1563e1f3c5ecfb91c511191eb409b

--- validations_libs/tests/test_utils.py
+++ validations_libs/tests/test_utils.py
@@ -13,7 +13,10 @@
 #   under the License.
 #
 
-from unittest import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 from unittest import TestCase
 
 from validations_libs import utils
