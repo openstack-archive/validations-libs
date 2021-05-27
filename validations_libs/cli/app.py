6ce045049dbc3ac20574643cb99d7ba9cb187176
Jiri Podivin <jpodivin@redhat.com>
2021-05-27 11:43:49 +0200

    Refactoring of the cli app module
    Logging strings use format function.
    
    Signed-off-by: Jiri Podivin <jpodivin@redhat.com>
    Change-Id: Ic380f878ed6a583ca1a32fc9b641535d868d6692

--- validations_libs/cli/app.py
+++ validations_libs/cli/app.py
@@ -41,12 +41,14 @@ class ValidationCliApp(App):
         self.LOG.debug('Initialize Validation App.')
 
     def prepare_to_run_command(self, cmd):
-        self.LOG.debug('prepare_to_run_command %s', cmd.__class__.__name__)
+        self.LOG.debug(
+            'prepare_to_run_command {}'.format(cmd.__class__.__name__))
 
     def clean_up(self, cmd, result, err):
-        self.LOG.debug('clean_up %s', cmd.__class__.__name__)
+        self.LOG.debug(
+            'clean_up {}'.format(cmd.__class__.__name__))
         if err:
-            self.LOG.debug('got an error: %s', err)
+            self.LOG.debug('got an error: {}'.format(err))
 
 
 def main(argv=sys.argv[1:]):
