29b147e431e101f4de82e01bc2634a08b290bde2
matbu <mat.bultel@gmail.com>
2021-06-03 18:43:45 +0200

    Override cliff command parser to handle argparser conflict_handler
    The argparse conflict_handler is hardcoded in Cliff, this patch
    override the command.Command().get_parser from Cliff in order
    to change the value.
    
    The conflict_handler from argparse is important for the VF because
    it's allow callers to override VF CLI args.
    
    Change-Id: I72524eeb3fc1ac0f384dcef25de61089ff15f67f

---
+++ validations_libs/cli/base.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+#   Copyright 2021 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+
+
+from cliff import _argparse
+from cliff.command import Command
+from cliff.lister import Lister
+
+
+class BaseCommand(Command):
+    """Base Command client implementation class"""
+
+    def get_parser(self, prog_name):
+        """Argument parser for base command"""
+        parser = _argparse.ArgumentParser(
+            description=self.get_description(),
+            epilog=self.get_epilog(),
+            prog=prog_name,
+            formatter_class=_argparse.SmartHelpFormatter,
+            conflict_handler='resolve',
+        )
+        for hook in self._hooks:
+            hook.obj.get_parser(parser)
+        return parser
+
+
+class BaseLister(Lister):
+    """Base Lister client implementation class"""
+
+    def get_parser(self, prog_name):
+        """Argument parser for base lister"""
+        parser = super(BaseLister, self).get_parser(prog_name)
+        vf_parser = _argparse.ArgumentParser(
+            description=self.get_description(),
+            epilog=self.get_epilog(),
+            prog=prog_name,
+            formatter_class=_argparse.SmartHelpFormatter,
+            conflict_handler='resolve',
+        )
+
+        for action in parser._actions:
+            vf_parser._add_action(action)
+
+        return vf_parser
