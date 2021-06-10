openstack-archive/validations-libs

commit f2830c52446b1bd57383e91e6eb3da84cfdab1c7

Improved log path handling
Validations logging now has several documented fallbacks.
constants.py now includes constant default path,
in order to improve readability and provide reference.

Tests are included.

Complementary patch for validations_commons callbacks
is optional, as the behavior should stay largely the same.

Co-Authored-By: mbu <mat.bultel@gmail.com>
Change-Id: Iab5f246f22fa6998ea6cec578eec6ebb5de13ad4

validations_libs/utils.py
@@ -32,25 +32,51 @@ def current_time():
     return '%sZ' % datetime.datetime.utcnow().isoformat()
 
 
-def create_artifacts_dir(dir_path=None, prefix=None):
-    """Create Ansible artifacts directory
+def create_log_dir(log_path=constants.VALIDATIONS_LOG_BASEDIR):
+    """Create default validations log dir.
+    Log the failure if encountering OSError or PermissionError.
+    :raises: RuntimeError
+    """
+    try:
+        if os.path.exists(log_path):
+            if os.access(log_path, os.W_OK):
+                return log_path
+            else:
+                create_log_dir(constants.VALIDATIONS_LOG_BASEDIR)
+        else:
+            os.makedirs(log_path)
+            return log_path
+    except (OSError, PermissionError):
+        LOG.error(
+            (
+                "Error while creating the log directory. "
+                "Please check the access rights for: '{}'"
+            ).format(log_path)
+        )
+        # Fallback in default path if log_path != from constants path
+        if log_path != constants.VALIDATIONS_LOG_BASEDIR:
+            create_log_dir(constants.VALIDATIONS_LOG_BASEDIR)
+        raise RuntimeError()
 
-    :param dir_path: Directory asbolute path
-    :type dir_path: `string`
+
+def create_artifacts_dir(log_path=constants.VALIDATIONS_LOG_BASEDIR,
+                         prefix=None):
+    """Create Ansible artifacts directory
+    :param log_path: Directory asbolute path
+    :type log_path: `string`
     :param prefix: Playbook name
     :type prefix: `string`
     :return: The UUID of the validation and the absolute Path of the log file
     :rtype: `string`, `string`
     """
-    dir_path = (dir_path if dir_path else
-                constants.VALIDATION_ANSIBLE_ARTIFACT_PATH)
+    artifact_dir = os.path.join(log_path, 'artifacts')
     validation_uuid = str(uuid.uuid4())
-    log_dir = "{}/{}_{}_{}".format(dir_path, validation_uuid,
+    log_dir = "{}/{}_{}_{}".format(artifact_dir, validation_uuid,
                                    (prefix if prefix else ''), current_time())
     try:
         os.makedirs(log_dir)
         return validation_uuid, log_dir
-    except OSError:
+    except (OSError, PermissionError):
         LOG.exception(
             (
                 "Error while creating Ansible artifacts log file."

validations_libs/tests/test_utils.py
@@ -47,18 +47,6 @@ class TestUtils(TestCase):
                           utils.get_validations_data,
                           validation)
 
-    @mock.patch('validations_libs.utils.current_time',
-                return_value='2020-04-02T06:58:20.352272Z')
-    @mock.patch('os.makedirs')
-    @mock.patch('uuid.uuid4', return_value='1234')
-    def test_create_artifacts_dir(self, mock_uuid, mock_makedirs,
-                                  mock_datetime):
-        uuid, dir_path = utils.create_artifacts_dir(dir_path='/tmp/foo',
-                                                    prefix='ntp')
-        self.assertEqual(uuid, '1234')
-        self.assertEqual(dir_path,
-                         '/tmp/foo/1234_ntp_2020-04-02T06:58:20.352272Z')
-
     @mock.patch('yaml.safe_load', return_value=fakes.FAKE_PLAYBOOK)
     @mock.patch('six.moves.builtins.open')
     @mock.patch('glob.glob')

validations_libs/tests/test_validation_actions.py
@@ -45,11 +45,9 @@ class TestValidationActions(TestCase):
                                               'My Validation Two Name',
                                              ['prep', 'pre-introspection'])]))
 
-    @mock.patch('validations_libs.utils.create_artifacts_dir',
-                return_value=('1234', '/tmp/'))
     @mock.patch('validations_libs.utils.get_validations_playbook',
                 return_value=['/tmp/foo/fake.yaml'])
-    def test_validation_skip_validation(self, mock_validation_play, mock_tmp):
+    def test_validation_skip_validation(self, mock_validation_play):
 
         playbook = ['fake.yaml']
         inventory = 'tmp/inventory.yaml'
@@ -66,16 +64,29 @@ class TestValidationActions(TestCase):
                                          limit_hosts=None)
         self.assertEqual(run_return, [])
 
+    @mock.patch('validations_libs.utils.current_time',
+                return_value='time')
+    @mock.patch('validations_libs.utils.uuid.uuid4',
+                return_value='123')
+    @mock.patch('validations_libs.utils.os.makedirs')
+    @mock.patch('validations_libs.utils.os.access',
+                return_value=True)
+    @mock.patch('validations_libs.utils.os.path.exists',
+                return_value=True)
     @mock.patch('validations_libs.utils.get_validations_playbook',
                 return_value=['/tmp/foo/fake.yaml'])
     @mock.patch('validations_libs.ansible.Ansible.run')
-    @mock.patch('validations_libs.utils.create_artifacts_dir',
-                return_value=('1234', '/tmp/'))
-    def test_validation_skip_on_specific_host(self, mock_tmp, mock_ansible_run,
-                                              mock_validation_play):
+    def test_validation_skip_on_specific_host(self, mock_ansible_run,
+                                              mock_validation_play,
+                                              mock_exists,
+                                              mock_access,
+                                              mock_makedirs,
+                                              mock_uuid,
+                                              mock_time):
+
         mock_ansible_run.return_value = ('fake.yaml', 0, 'successful')
         run_called_args = {
-            'workdir': '/tmp/',
+            'workdir': '/var/log/validations/artifacts/123_fake.yaml_time',
             'playbook': '/tmp/foo/fake.yaml',
             'base_dir': '/usr/share/ansible/',
             'playbook_dir': '/tmp/foo',
@@ -86,11 +97,11 @@ class TestValidationActions(TestCase):
             'quiet': True,
             'extra_vars': None,
             'limit_hosts': '!cloud1',
-            'ansible_artifact_path': '/tmp/',
             'extra_env_variables': None,
             'ansible_cfg': None,
             'gathering_policy': 'explicit',
-            'log_path': None,
+            'ansible_artifact_path': '/var/log/validations/artifacts/123_fake.yaml_time',
+            'log_path': '/var/log/validations',
             'run_async': False,
             'python_interpreter': None,
             'ssh_user': None
@@ -106,22 +117,36 @@ class TestValidationActions(TestCase):
 
         run = ValidationActions()
         run_return = run.run_validations(playbook, inventory,
+                                         log_path='/var/log/validations',
                                          validations_dir='/tmp/foo',
                                          skip_list=skip_list,
-                                         limit_hosts='!cloud1',
-                                         )
+                                         limit_hosts='!cloud1')
+
         mock_ansible_run.assert_called_with(**run_called_args)
 
+    @mock.patch('validations_libs.utils.current_time',
+                return_value='time')
+    @mock.patch('validations_libs.utils.uuid.uuid4',
+                return_value='123')
+    @mock.patch('validations_libs.utils.os.makedirs')
+    @mock.patch('validations_libs.utils.os.access',
+                return_value=True)
+    @mock.patch('validations_libs.utils.os.path.exists',
+                return_value=True)
     @mock.patch('validations_libs.utils.get_validations_playbook',
                 return_value=['/tmp/foo/fake.yaml'])
     @mock.patch('validations_libs.ansible.Ansible.run')
-    @mock.patch('validations_libs.utils.create_artifacts_dir',
-                return_value=('1234', '/tmp/'))
-    def test_validation_skip_with_limit_host(self, mock_tmp, mock_ansible_run,
-                                             mock_validation_play):
+    def test_validation_skip_with_limit_host(self, mock_ansible_run,
+                                             mock_validation_play,
+                                             mock_exists,
+                                             mock_access,
+                                             mock_makedirs,
+                                             mock_uuid,
+                                             mock_time):
+
         mock_ansible_run.return_value = ('fake.yaml', 0, 'successful')
         run_called_args = {
-            'workdir': '/tmp/',
+            'workdir': '/var/log/validations/artifacts/123_fake.yaml_time',
             'playbook': '/tmp/foo/fake.yaml',
             'base_dir': '/usr/share/ansible/',
             'playbook_dir': '/tmp/foo',
@@ -135,8 +160,8 @@ class TestValidationActions(TestCase):
             'extra_env_variables': None,
             'ansible_cfg': None,
             'gathering_policy': 'explicit',
-            'ansible_artifact_path': '/tmp/',
-            'log_path': None,
+            'ansible_artifact_path': '/var/log/validations/artifacts/123_fake.yaml_time',
+            'log_path': '/var/log/validations',
             'run_async': False,
             'python_interpreter': None,
             'ssh_user': None
@@ -152,40 +177,31 @@ class TestValidationActions(TestCase):
 
         run = ValidationActions()
         run_return = run.run_validations(playbook, inventory,
+                                         log_path='/var/log/validations',
                                          validations_dir='/tmp/foo',
                                          skip_list=skip_list,
                                          limit_hosts='cloud,cloud1,!cloud2')
+
         mock_ansible_run.assert_called_with(**run_called_args)
 
-    @mock.patch('validations_libs.validation_logs.ValidationLogs.get_results')
+    @mock.patch('validations_libs.validation_actions.ValidationLogs.get_results',
+                side_effect=fakes.FAKE_SUCCESS_RUN)
     @mock.patch('validations_libs.utils.parse_all_validations_on_disk')
     @mock.patch('validations_libs.ansible.Ansible.run')
-    @mock.patch('validations_libs.utils.create_artifacts_dir',
-                return_value=('1234', '/tmp/'))
-    def test_validation_run_success(self, mock_tmp, mock_ansible_run,
-                                    mock_validation_dir, mock_results):
+    def test_validation_run_success(self, mock_ansible_run,
+                                    mock_validation_dir,
+                                    mock_results):
+
         mock_validation_dir.return_value = [{
             'description': 'My Validation One Description',
             'groups': ['prep', 'pre-deployment'],
             'id': 'foo',
             'name': 'My Validition One Name',
             'parameters': {}}]
+
         mock_ansible_run.return_value = ('foo.yaml', 0, 'successful')
 
-        mock_results.return_value = [{'Duration': '0:00:01.761',
-                                      'Host_Group': 'overcloud',
-                                      'Status': 'PASSED',
-                                      'Status_by_Host': 'subnode-1,PASSED',
-                                      'UUID': 'foo',
-                                      'Unreachable_Hosts': '',
-                                      'Validations': 'ntp'}]
-        expected_run_return = [{'Duration': '0:00:01.761',
-                                'Host_Group': 'overcloud',
-                                'Status': 'PASSED',
-                                'Status_by_Host': 'subnode-1,PASSED',
-                                'UUID': 'foo',
-                                'Unreachable_Hosts': '',
-                                'Validations': 'ntp'}]
+        expected_run_return = fakes.FAKE_SUCCESS_RUN[0]
 
         playbook = ['fake.yaml']
         inventory = 'tmp/inventory.yaml'
@@ -209,17 +225,18 @@ class TestValidationActions(TestCase):
     @mock.patch('validations_libs.validation_logs.ValidationLogs.get_results')
     @mock.patch('validations_libs.utils.parse_all_validations_on_disk')
     @mock.patch('validations_libs.ansible.Ansible.run')
-    @mock.patch('validations_libs.utils.create_artifacts_dir',
-                return_value=('1234', '/tmp/'))
-    def test_validation_run_failed(self, mock_tmp, mock_ansible_run,
+    def test_validation_run_failed(self, mock_ansible_run,
                                    mock_validation_dir, mock_results):
+
         mock_validation_dir.return_value = [{
             'description': 'My Validation One Description',
             'groups': ['prep', 'pre-deployment'],
             'id': 'foo',
             'name': 'My Validition One Name',
             'parameters': {}}]
+
         mock_ansible_run.return_value = ('foo.yaml', 0, 'failed')
+
         mock_results.return_value = [{'Duration': '0:00:01.761',
                                       'Host_Group': 'overcloud',
                                       'Status': 'PASSED',
@@ -227,6 +244,7 @@ class TestValidationActions(TestCase):
                                       'UUID': 'foo',
                                       'Unreachable_Hosts': '',
                                       'Validations': 'ntp'}]
+
         expected_run_return = [{'Duration': '0:00:01.761',
                                 'Host_Group': 'overcloud',
                                 'Status': 'PASSED',

validations_libs/constants.py
@@ -20,9 +20,7 @@ ANSIBLE_VALIDATION_DIR = '/usr/share/ansible/validation-playbooks'
 
 VALIDATION_GROUPS_INFO = '%s/groups.yaml' % DEFAULT_VALIDATIONS_BASEDIR
 
-VALIDATIONS_LOG_BASEDIR = ('/var/log/validations'
-                           if os.path.exists('/var/log/validations') else
-                           os.getcwd())
+VALIDATIONS_LOG_BASEDIR = os.path.join(os.environ.get('HOME'), 'validations')
 
 VALIDATION_ANSIBLE_ARTIFACT_PATH = '{}/artifacts/'.format(
     VALIDATIONS_LOG_BASEDIR)

commit 48cc737446462370ca896c61743089c897eca539

Fixing the recursive path handling.
The patch f2830c52446b1bd57383e91e6eb3da84cfdab1c7 introduced
several potentially breaking bugs, which have to be fixed.
Namely, several of the termination conditionals lacked proper
return statements.

In addition to the fix of the previously describe issues
the following changes are included to prevent more arising in
the future.

Every major behavioral branch now has a dedicated log statement.
Test coverage included.
Expanded docstrings.
Increased expressiveness of var names.

Signed-off-by: Jiri Podivin <jpodivin@redhat.com>
Change-Id: Ie7471361acf1f4ae197e76b22eb7ba4d3f62fe92

validations_libs/utils.py
@@ -33,56 +33,97 @@ def current_time():
 
 
 def create_log_dir(log_path=constants.VALIDATIONS_LOG_BASEDIR):
-    """Create default validations log dir.
+    """Check for presence of the selected validations log dir.
+    Create the directory if needed, and use fallback if that
+    proves too tall an order.
+
     Log the failure if encountering OSError or PermissionError.
-    :raises: RuntimeError
+
+    :param log_path: path of the selected log directory
+    :type log_path: `string`
+    :return: valid path to the log directory
+    :rtype: `string`
+
+    :raises: RuntimeError if even the fallback proves unavailable.
     """
     try:
         if os.path.exists(log_path):
             if os.access(log_path, os.W_OK):
                 return log_path
             else:
-                create_log_dir(constants.VALIDATIONS_LOG_BASEDIR)
+                LOG.error(
+                    (
+                        "Selected log directory '{log_path}' is inaccessible. "
+                        "Please check the access rights for: '{log_path}'"
+                    ).format(
+                        log_path=log_path))
+                if log_path != constants.VALIDATIONS_LOG_BASEDIR:
+                    LOG.warning(
+                    (
+                        "Resorting to the preset '{default_log_path}'"
+                    ).format(
+                        default_log_path=constants.VALIDATIONS_LOG_BASEDIR))
+
+                    return create_log_dir()
+                else:
+                    raise RuntimeError()
         else:
+            LOG.warning(
+                (
+                    "Selected log directory '{log_path}' does not exist. "
+                    "Attempting to create it."
+                ).format(
+                    log_path=log_path))
+
             os.makedirs(log_path)
             return log_path
-    except (OSError, PermissionError):
+    except (OSError, PermissionError) as error:
         LOG.error(
             (
-                "Error while creating the log directory. "
-                "Please check the access rights for: '{}'"
-            ).format(log_path)
-        )
+                "Encountered an {error} while creating the log directory. "
+                "Please check the access rights for: '{log_path}'"
+            ).format(
+                error=error,
+                log_path=log_path))
+
         # Fallback in default path if log_path != from constants path
         if log_path != constants.VALIDATIONS_LOG_BASEDIR:
-            create_log_dir(constants.VALIDATIONS_LOG_BASEDIR)
+            LOG.debug(
+                (
+                    "Resorting to the preset '{default_log_path}'."
+                ).format(
+                    default_log_path=constants.VALIDATIONS_LOG_BASEDIR))
+
+            return create_log_dir()
         raise RuntimeError()
 
 
 def create_artifacts_dir(log_path=constants.VALIDATIONS_LOG_BASEDIR,
-                         prefix=None):
-    """Create Ansible artifacts directory
+                         prefix=''):
+    """Create Ansible artifacts directory for the validation run
     :param log_path: Directory asbolute path
     :type log_path: `string`
     :param prefix: Playbook name
     :type prefix: `string`
-    :return: The UUID of the validation and the absolute path of the log file
+    :return: UUID of the validation run, absolute path of the validation artifacts directory
     :rtype: `string`, `string`
     """
     artifact_dir = os.path.join(log_path, 'artifacts')
     validation_uuid = str(uuid.uuid4())
-    log_dir = "{}/{}_{}_{}".format(artifact_dir, validation_uuid,
-                                   (prefix if prefix else ''), current_time())
+    validation_artifacts_dir = "{}/{}_{}_{}".format(
+        artifact_dir,
+        validation_uuid,
+        prefix,
+        current_time())
     try:
-        os.makedirs(log_dir)
-        return validation_uuid, log_dir
+        os.makedirs(validation_artifacts_dir)
+        return validation_uuid, validation_artifacts_dir
     except (OSError, PermissionError):
         LOG.exception(
             (
-                "Error while creating Ansible artifacts directory. "
-                "Please check the access rights for: '{}'"
-            ).format(log_dir)
-        )
+                "Error while creating Ansible artifacts log file. "
+                "Please check the access rights for '{}'"
+            ).format(validation_artifacts_dir))
 
         raise RuntimeError()
 

validations_libs/tests/fakes.py
@@ -13,6 +13,8 @@
 #   under the License.
 #
 
+from validations_libs import constants
+
 VALIDATIONS_LIST = [{
     'description': 'My Validation One Description',
     'groups': ['prep', 'pre-deployment'],
@@ -300,3 +302,9 @@ FAKE_VALIDATIONS_PATH = '/usr/share/ansible/validation-playbooks'
 
 def fake_ansible_runner_run_return(status='successful', rc=0):
     return status, rc
+
+
+def _accept_default_log_path(path, *args):
+    if path == constants.VALIDATIONS_LOG_BASEDIR:
+        return True
+    return False

validations_libs/tests/test_utils.py
@@ -17,9 +17,10 @@ try:
     from unittest import mock
 except ImportError:
     import mock
+
 from unittest import TestCase
 
-from validations_libs import utils
+from validations_libs import utils, constants
 from validations_libs.tests import fakes
 
 
@@ -211,3 +212,99 @@ class TestUtils(TestCase):
         self.assertRaises(TypeError,
                           utils.convert_data,
                           data=data_dict)
+
+    @mock.patch('validations_libs.utils.LOG', autospec=True)
+    @mock.patch('validations_libs.utils.os.makedirs')
+    @mock.patch(
+        'validations_libs.utils.os.access',
+        side_effect=[False, True])
+    @mock.patch('validations_libs.utils.os.path.exists', return_value=True)
+    def test_create_log_dir_access_issue(self, mock_exists,
+                                         mock_access, mock_mkdirs,
+                                         mock_log):
+        log_path = utils.create_log_dir("/foo/bar")
+        self.assertEqual(log_path, constants.VALIDATIONS_LOG_BASEDIR)
+
+    @mock.patch('validations_libs.utils.LOG', autospec=True)
+    @mock.patch(
+        'validations_libs.utils.os.makedirs',
+        side_effect=PermissionError)
+    @mock.patch(
+        'validations_libs.utils.os.access',
+        autospec=True,
+        return_value=True)
+    @mock.patch(
+        'validations_libs.utils.os.path.exists',
+        autospec=True,
+        side_effect=fakes._accept_default_log_path)
+    def test_create_log_dir_existence_issue(self, mock_exists,
+                                            mock_access, mock_mkdirs,
+                                            mock_log):
+        """Tests behavior after encountering non-existence
+        of the the selected log folder, failed attempt to create it
+        (raising PermissionError), and finally resorting to a fallback.
+        """
+        log_path = utils.create_log_dir("/foo/bar")
+        self.assertEqual(log_path, constants.VALIDATIONS_LOG_BASEDIR)
+
+    @mock.patch('validations_libs.utils.LOG', autospec=True)
+    @mock.patch('validations_libs.utils.os.makedirs')
+    @mock.patch('validations_libs.utils.os.access', return_value=True)
+    @mock.patch('validations_libs.utils.os.path.exists', return_value=True)
+    def test_create_log_dir_success(self, mock_exists,
+                                    mock_access, mock_mkdirs,
+                                    mock_log):
+        """Test successful log dir retrieval on the first try.
+        """
+        log_path = utils.create_log_dir("/foo/bar")
+        self.assertEqual(log_path, "/foo/bar")
+
+    @mock.patch('validations_libs.utils.LOG', autospec=True)
+    @mock.patch(
+        'validations_libs.utils.os.makedirs',
+        side_effect=PermissionError)
+    @mock.patch('validations_libs.utils.os.access', return_value=False)
+    @mock.patch('validations_libs.utils.os.path.exists', return_value=False)
+    def test_create_log_dir_runtime_err(self, mock_exists,
+                                        mock_access, mock_mkdirs,
+                                        mock_log):
+        """Test if failure of the fallback raises 'RuntimeError'
+        """
+        self.assertRaises(RuntimeError, utils.create_log_dir, "/foo/bar")
+
+    @mock.patch('validations_libs.utils.LOG', autospec=True)
+    @mock.patch(
+        'validations_libs.utils.os.makedirs',
+        side_effect=PermissionError)
+    @mock.patch('validations_libs.utils.os.access', return_value=False)
+    @mock.patch(
+        'validations_libs.utils.os.path.exists',
+        side_effect=fakes._accept_default_log_path)
+    def test_create_log_dir_default_perms_runtime_err(
+                                        self, mock_exists,
+                                        mock_access, mock_mkdirs,
+                                        mock_log):
+        """Test if the inaccessible fallback raises 'RuntimeError'
+        """
+        self.assertRaises(RuntimeError, utils.create_log_dir, "/foo/bar")
+
+    @mock.patch('validations_libs.utils.LOG', autospec=True)
+    @mock.patch('validations_libs.utils.os.makedirs')
+    @mock.patch('validations_libs.utils.os.access', return_value=False)
+    @mock.patch('validations_libs.utils.os.path.exists', return_value=False)
+    def test_create_log_dir_mkdirs(self, mock_exists,
+                                   mock_access, mock_mkdirs,
+                                   mock_log):
+        """Test successful creation of the directory if the first access fails.
+        """
+
+        log_path = utils.create_log_dir("/foo/bar")
+        self.assertEqual(log_path, "/foo/bar")
+
+    @mock.patch(
+        'validations_libs.utils.os.makedirs',
+        side_effect=PermissionError)
+    def test_create_artifacts_dir_runtime_err(self, mock_mkdirs):
+        """Test if failure to create artifacts dir raises 'RuntimeError'.
+        """
+        self.assertRaises(RuntimeError, utils.create_artifacts_dir, "/foo/bar")

validations_libs/tests/test_validation_actions.py
@@ -45,9 +45,11 @@ class TestValidationActions(TestCase):
                                               'My Validation Two Name',
                                              ['prep', 'pre-introspection'])]))
 
+    @mock.patch('validations_libs.utils.os.access', return_value=True)
+    @mock.patch('validations_libs.utils.os.path.exists', return_value=True)
     @mock.patch('validations_libs.utils.get_validations_playbook',
                 return_value=['/tmp/foo/fake.yaml'])
-    def test_validation_skip_validation(self, mock_validation_play):
+    def test_validation_skip_validation(self, mock_validation_play, mock_exists, mock_access):
 
         playbook = ['fake.yaml']
         inventory = 'tmp/inventory.yaml'
@@ -184,13 +186,17 @@ class TestValidationActions(TestCase):
 
         mock_ansible_run.assert_called_with(**run_called_args)
 
+    @mock.patch('validations_libs.utils.os.makedirs')
+    @mock.patch('validations_libs.utils.os.access', return_value=True)
+    @mock.patch('validations_libs.utils.os.path.exists', return_value=True)
     @mock.patch('validations_libs.validation_actions.ValidationLogs.get_results',
                 side_effect=fakes.FAKE_SUCCESS_RUN)
     @mock.patch('validations_libs.utils.parse_all_validations_on_disk')
     @mock.patch('validations_libs.ansible.Ansible.run')
     def test_validation_run_success(self, mock_ansible_run,
                                     mock_validation_dir,
-                                    mock_results):
+                                    mock_results, mock_exists, mock_access,
+                                    mock_makedirs):
 
         mock_validation_dir.return_value = [{
             'description': 'My Validation One Description',
@@ -222,11 +228,16 @@ class TestValidationActions(TestCase):
                           validations_dir='/tmp/foo'
                           )
 
+    @mock.patch('validations_libs.utils.os.makedirs')
+    @mock.patch('validations_libs.utils.os.access', return_value=True)
+    @mock.patch('validations_libs.utils.os.path.exists', return_value=True)
     @mock.patch('validations_libs.validation_logs.ValidationLogs.get_results')
     @mock.patch('validations_libs.utils.parse_all_validations_on_disk')
     @mock.patch('validations_libs.ansible.Ansible.run')
     def test_validation_run_failed(self, mock_ansible_run,
-                                   mock_validation_dir, mock_results):
+                                   mock_validation_dir, mock_results,
+                                   mock_exists, mock_access,
+                                   mock_makedirs):
 
         mock_validation_dir.return_value = [{
             'description': 'My Validation One Description',